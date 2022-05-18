876946bb2312e225ce1f81453ada6703476b2fac
MishaDobrynin <77999860+MishaDobrynin@users.noreply.github.com>
2022-05-18 15:29:54 -0700

    Improvement
    Higher framerate update

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
+import timeit
+
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 from reader import tup
-class Voxel(Entity):
+class Voxel(Button):
     def __init__(self, position=(0,0,0)):
-        global array
+        global array,terrain
         super().__init__(
-            parent = scene,
+            parent = terrain,
             position = position,
             model = 'cube',
             origin_y = .5,
-            texture = 'white_cube',
+            texture = 'oak.jpg',
             color = color.color(0, 0, random.uniform(.9, 1.0)),
-            highlight_color = color.lime,
-            collider = 'cube',
+            highlight_color = color.white,
         )
         array.append((self.position[0], self.position[1], self.position[2]))
     def input(self, key):
@@ -34,8 +35,10 @@ print("Loading world...")
 array = tup(open('locations.txt', 'r'))
 app = Ursina()
 HoverMode = False
+terrain = Entity(parent = scene, model = None)
 for i in range(len(array)):
     Voxel(position = array[i])
+terrain.collider = 'mesh'
 player = FirstPersonController()
 WORLD = 20
 def input(key):
@@ -43,7 +46,6 @@ def input(key):
         fil = open('locations.txt', 'w')
         for el in array:
             fil.write(str(el) + "\n")
-
 # def input(key):
 #     if key == 'left mouse down':
 #         hit_info = raycast(camera.world_position, camera.forward, distance=10)
@@ -51,4 +53,5 @@ def input(key):
 #             Voxel(position=hit_info.entity.position + hit_info.normal)
 Entity(model = 'sphere', color = color.azure, double_sided = True, scale = 150)
 player = FirstPersonController()
+player.gravity = 1
 app.run()
